ad1a4ae29ed510dff64c2424cb68fe724a66fcfd
Angela Wang <ranran.wang@wework.com>
2018-02-09 00:50:28 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,9 +1,7 @@
 # Build your say_hello method here
 
-def say_hello(name)
- puts"Hello #{name}!"
-end 
+say_hello = "Garbiela"
 
-def puts say_hello()
- puts"Hello Ruby Programmer!"
+def say_hello(name = "Ruby Programmer")
+ puts"Hello #{name}!"
 end 
